d4af93f554b6616894de35b2d7b3dbb323dfba01
Rory Campbell <roryc89@googlemail.com>
2023-01-12 21:37:18 +0100

    remove with statements from build

--- build/templates/mht_part.js
+++ build/templates/mht_part.js
@@ -1,32 +1,34 @@
 var _ = {};
 var escapeMap = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#x27;'
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': "&quot;",
+  "'": "&#x27;",
 };
-var escapeRegexp = new RegExp('[' + Object.keys(escapeMap).join('') + ']', 'g');
-_.escape = function(string) {
-    if (!string) return '';
-    return String(string).replace(escapeRegexp, function(match) {
-        return escapeMap[match];
-    });
+var escapeRegexp = new RegExp("[" + Object.keys(escapeMap).join("") + "]", "g");
+_.escape = function (string) {
+  if (!string) return "";
+  return String(string).replace(escapeRegexp, function (match) {
+    return escapeMap[match];
+  });
 };
-module.exports = function(obj) {
-obj || (obj = {});
-var __t, __p = '', __e = _.escape;
-with (obj) {
-__p += '------=mhtDocumentPart\nContent-Type: ' +
-((__t = ( contentType )) == null ? '' : __t) +
-'\nContent-Transfer-Encoding: ' +
-((__t = ( contentEncoding )) == null ? '' : __t) +
-'\nContent-Location: ' +
-((__t = ( contentLocation )) == null ? '' : __t) +
-'\n\n' +
-((__t = ( encodedContent )) == null ? '' : __t) +
-'\n';
+module.exports = function (obj) {
+  obj || (obj = {});
+  var __t,
+    __p = "",
+    __e = _.escape;
+  const { contentType, contentEncoding, contentLocation, encodedContent } = obj;
+  __p +=
+    "------=mhtDocumentPart\nContent-Type: " +
+    ((__t = contentType) == null ? "" : __t) +
+    "\nContent-Transfer-Encoding: " +
+    ((__t = contentEncoding) == null ? "" : __t) +
+    "\nContent-Location: " +
+    ((__t = contentLocation) == null ? "" : __t) +
+    "\n\n" +
+    ((__t = encodedContent) == null ? "" : __t) +
+    "\n";
 
-}
-return __p
-}
+  return __p;
+};
